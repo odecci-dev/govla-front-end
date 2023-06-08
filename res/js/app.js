b2efd60d7d13c326a69edfa61d13e9757993fab3
Odecci-Dev2 <dev2@odecci.com>
2023-06-08 22:40:34 +0800

    Fixed some minor navigation bugs

--- res/js/app.js
+++ res/js/app.js
@@ -1,47 +1,51 @@
-// * Submenu Toggle
-document.addEventListener("click", e => {
+function navigationEvents() {
 
-    const dashboard = document.querySelector(".main-dashboard")
+    // * Submenu Toggle
+    document.addEventListener("click", e => {
 
-    // * When a link is clicked, toggle the submenu
-    const isDropdownButton = e.target.matches("[data-dropdown-button]")
+        const dashboard = document.querySelector(".main-dashboard")
 
-    // * Ignore the click in the submenus (to not close when the dropdown is clicked inside the submenu)
-    if (!isDropdownButton && e.target.closest("[data-dropdown]") != null) return
+        // * When a link is clicked, toggle the submenu
+        const isDropdownButton = e.target.matches("[data-dropdown-button]")
+
+        // * Ignore the click in the submenus (to not close when the dropdown is clicked inside the submenu)
+        if (!isDropdownButton && e.target.closest("[data-dropdown]") != null) return
 
 
 
-    // * If this is a dropdown button, I want toggle submenu by clicking
-    let currentDropdown
-    if (isDropdownButton) {
+        // * If this is a dropdown button, I want toggle submenu by clicking
+        let currentDropdown
+        if (isDropdownButton) {
 
-        currentDropdown = e.target.closest("[data-dropdown]")
-            // * Hide or Show submenu toggle
-        currentDropdown.classList.toggle("showMenu")
+            currentDropdown = e.target.closest("[data-dropdown]")
+                // * Hide or Show submenu toggle
+            currentDropdown.classList.toggle("showMenu")
+                // dashboard.style.zIndex = 10
 
-    }
-
-    // * Get rid off the submenus that are not open (closing submenus that are not clicked)
-
-    // * Getting all the submenus and looping in each one and close them
-    document.querySelectorAll("[data-dropdown].showMenu").forEach(dropdown => {
-
-        // * If this dropdown is equal to the current dropdown then add a z-index of 1 to prevent overlapping...   
-        if (dropdown === currentDropdown) {
-            dropdown.style.zIndex = 9
-            dashboard.style.zIndex = -1
-            return
         }
-        // * Otherwise, close it
-        dropdown.classList.remove("showMenu")
-        dropdown.style.zIndex = ''
-        dashboard.style.zIndex = ''
 
-        return
+        // * Get rid off the submenus that are not open (closing submenus that are not clicked)
+
+        // * Getting all the submenus and looping in each one and close them
+        document.querySelectorAll("[data-dropdown].showMenu").forEach(dropdown => {
+
+            // * If this dropdown is equal to the current dropdown then add a z-index of 1 to prevent overlapping...   
+            if (dropdown === currentDropdown) {
+                dropdown.style.zIndex = 9
+                dashboard.style.zIndex = -1
+                return
+            }
+            // * Otherwise, close it
+            dropdown.classList.remove("showMenu")
+            dashboard.style.zIndex = 10
+            return
+
+        })
 
     })
 
-})
+}
+
 
 
 // * Table Checkbox Toggle
@@ -58,4 +62,9 @@ function checkAll(tableCheckBox) {
             checkbox.checked = false;
         })
     }
+}
+
+
+window.onload = function() {
+    navigationEvents();
 }
